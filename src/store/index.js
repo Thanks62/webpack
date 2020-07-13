a32105b49090da0147d298922361d261d1d221ca
xieyue <xieyue@huya.com>
2020-07-13 15:55:24 +0800

    redux dev tool config

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import {createStore} from 'redux'
 //import {combineReducers} from 'redux'
 import todos from '../reducers/todos'
-//const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-let store=createStore(todos);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let store=createStore(todos,composeEnhancers());
 export default store;
 
