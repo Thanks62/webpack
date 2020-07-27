058192c5944216df9475a465b0977298ce3b5712
xieyue <xieyue@huya.com>
2020-07-27 16:04:23 +0800

    http-proxy-middleware & mySQL数据库100%

--- app.js
+++ app.js
@@ -4,33 +4,49 @@ var express = require('express');
 var path = require('path');
 var ejs = require('ejs');
 const bodyParser=require('body-parser');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const option={
+    target:'http://localhost:8888',
+    changeOrigin:true
+}
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-// 对所有(/)URL或路由返回index.html 
-app.use('',function(req,res,next){
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods','POST')
+// 对/getData使用允许跨域响应头
+app.use('/getData',function(req,res,next){
+    res.setHeader('Access-Control-Allow-Origin','http://172.21.19.103:8888');
     next();
 })
+// 对/getData使用允许跨域中间件
+// app.use('/getData',function(req,res,next){
+//     createProxyMiddleware(option);
+//     next();
+// })
 app.get('/getData', function (req, res) {
+    //json数据库
     // const action=require('./service/action/getData');
     // action.excute(req,res);
+    //mySQL数据库
 	const db=require('./service/dataBase/db.js');
-	db.query('SELECT * FROM todo',[],function(result){
+	db.query('SELECT * FROM todo',function(result){
 		res.send(result);
-		console.log(result);
 	})
 });
 app.get('/', function (req, res) {
     res.render('index');
 });
 app.post('/getData',function(req,res){
-    const action=require('./service/action/getData');
+    //const action=require('./service/action/getData');
+    const db=require('./service/dataBase/db.js');
     switch (req.body.operation){
-        case "add":action.addData(req,res);break;
-        case "edit":action.editData(req,res);break;
-        case "delete":action.deleteData(req,res);break;
+        //json数据库
+        // case "add":action.addData(req,res);break;
+        // case "edit":action.editData(req,res);break;
+        // case "delete":action.deleteData(req,res);break;
+        //mySQL数据库
+        case "add":db.query('INSERT INTO `todo`(`id`, `name`, `job`) VALUES ("'+req.body.id+'","'+req.body.todo+'","'+req.body.time+'");');break;
+        case "edit":db.query('UPDATE `todo` SET `name`="'+req.body.todo+'",`job`="'+req.body.time+'" WHERE `id`="'+req.body.id+'"');break;
+        case "delete":db.query('DELETE from `todo` WHERE `id`="'+req.body.id+'"');break;
         default: break;
     }
     
--- service/dataBase/db.js
+++ service/dataBase/db.js
@@ -1,27 +1,28 @@
 const mysql=require('mysql');
-const connection=mysql.createConnection({
+const config={
 	host:'localhost',
 	user:'root',
 	port: '3306',
 	password:'123456',
 	database:'test'
-});
+};
 module.exports = {
-    query : function(sql,params,callback){
+    query : function(sql,callback){
+		const connection=mysql.createConnection(config);
         connection.connect(function(err){
             if(err){
-                console.log('Failed');
+                console.log('connect Failed');
                 throw err;
             }
-			connection.query( sql, params, function(err,results,fields ){
+			connection.query( sql, function(err,results){
 				if(err){
-					console.log('Failed');
+					console.log('query Failed');
 					throw err;
 				}
 				callback && callback(JSON.parse(JSON.stringify(results)));
 				connection.end(function(err){
 					if(err){
-						console.log('Failed');
+						console.log('close Failed');
 						throw err;
 					}
 				});
